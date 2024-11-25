545fcc58456f60f8add319bc416351cf5e11e9ea
Tianyi <Tianyyiii@gmail.com>
2024-11-25 15:51:38 -0500

    physical loss

--- neuraloperator/neuralop/losses/data_losses.py
+++ neuraloperator/neuralop/losses/data_losses.py
@@ -404,6 +404,268 @@ class H1Loss(object):
             normalization constant for reduction, by default None
         """
         return self.rel(y_pred, y, h=h)
+    
+class H1Loss_p(object):
+    """
+    H1Loss provides the H1 Sobolev norm between
+    two d-dimensional discretized functions
+    """
+    def __init__(self, d=1, L=2*math.pi, reduce_dims=0, reductions='sum', fix_x_bnd=False, fix_y_bnd=False, fix_z_bnd=False):
+        """
+
+        Parameters
+        ----------
+        d : int, optional
+            dimension of input functions, by default 1
+        L : int or list, optional
+            quadrature weights (single or by dimension), by default 2*math.pi
+        reduce_dims : int, optional
+            dimensions across which to reduce for loss, by default 0
+        reductions : str, optional
+            whether to reduce each dimension above 
+            by summing ('sum') or averaging ('mean')
+        fix_x_bnd : bool, optional
+            whether to fix finite difference derivative
+            computation on the x boundary, by default False
+        fix_y_bnd : bool, optional
+            whether to fix finite difference derivative
+            computation on the y boundary, by default False
+        fix_z_bnd : bool, optional
+            whether to fix finite difference derivative
+            computation on the z boundary, by default False
+        """
+        super().__init__()
+
+        assert d > 0 and d < 4, "Currently only implemented for 1, 2, and 3-D."
+
+        self.d = d
+        self.fix_x_bnd = fix_x_bnd
+        self.fix_y_bnd = fix_y_bnd
+        self.fix_z_bnd = fix_z_bnd
+
+        if isinstance(reduce_dims, int):
+            self.reduce_dims = [reduce_dims]
+        else:
+            self.reduce_dims = reduce_dims
+        
+        if self.reduce_dims is not None:
+            if isinstance(reductions, str):
+                assert reductions == 'sum' or reductions == 'mean'
+                self.reductions = [reductions]*len(self.reduce_dims)
+            else:
+                for j in range(len(reductions)):
+                    assert reductions[j] == 'sum' or reductions[j] == 'mean'
+                self.reductions = reductions
+
+        if isinstance(L, float):
+            self.L = [L]*self.d
+        else:
+            self.L = L
+    
+    @property
+    def name(self):
+        return f"H1_{self.d}DLoss"
+     
+    def compute_terms(self, x, y, h):
+        """compute_terms computes the necessary
+        finite-difference derivative terms for computing
+        the H1 norm
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            inputs
+        y : torch.Tensor
+            targets
+        h : int or list
+            discretization size (single or per dim)
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
+        dict_x = {}
+        dict_y = {}
+
+        if self.d == 1:
+            dict_x[0] = x
+            dict_y[0] = y
+
+            x_x = central_diff_1d(x, h[0], fix_x_bnd=self.fix_x_bnd)
+            y_x = central_diff_1d(y, h[0], fix_x_bnd=self.fix_x_bnd)
+
+            dict_x[1] = x_x
+            dict_y[1] = y_x
+        
+        elif self.d == 2:
+            dict_x[0] = torch.flatten(x, start_dim=-2)
+            dict_y[0] = torch.flatten(y, start_dim=-2)
+
+            x_x, x_y = central_diff_2d(x, h, fix_x_bnd=self.fix_x_bnd, fix_y_bnd=self.fix_y_bnd)
+            y_x, y_y = central_diff_2d(y, h, fix_x_bnd=self.fix_x_bnd, fix_y_bnd=self.fix_y_bnd)
+
+            dict_x[1] = torch.flatten(x_x, start_dim=-2)
+            dict_x[2] = torch.flatten(x_y, start_dim=-2)
+
+            dict_y[1] = torch.flatten(y_x, start_dim=-2)
+            dict_y[2] = torch.flatten(y_y, start_dim=-2)
+        
+        else:
+            dict_x[0] = torch.flatten(x, start_dim=-3)
+            dict_y[0] = torch.flatten(y, start_dim=-3)
+
+            x_x, x_y, x_z = central_diff_3d(x, h, fix_x_bnd=self.fix_x_bnd, fix_y_bnd=self.fix_y_bnd, fix_z_bnd=self.fix_z_bnd)
+            y_x, y_y, y_z = central_diff_3d(y, h, fix_x_bnd=self.fix_x_bnd, fix_y_bnd=self.fix_y_bnd, fix_z_bnd=self.fix_z_bnd)
+
+            dict_x[1] = torch.flatten(x_x, start_dim=-3)
+            dict_x[2] = torch.flatten(x_y, start_dim=-3)
+            dict_x[3] = torch.flatten(x_z, start_dim=-3)
+
+            dict_y[1] = torch.flatten(y_x, start_dim=-3)
+            dict_y[2] = torch.flatten(y_y, start_dim=-3)
+            dict_y[3] = torch.flatten(y_z, start_dim=-3)
+        
+        return dict_x, dict_y
+
+    def uniform_h(self, x):
+        """uniform_h creates default normalization constants
+        if none already exist.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            input data
+
+        Returns
+        -------
+        h : list
+            list of normalization constants per-dim
+        """
+        h = [0.0]*self.d
+        for j in range(self.d, 0, -1):
+            h[-j] = self.L[-j]/x.size(-j)
+        
+        return h
+    
+    def reduce_all(self, x):
+        """
+        reduce x across all dimensions in self.reduce_dims 
+        according to self.reductions
+
+        Params
+        ------
+        x: torch.Tensor
+            inputs
+        """
+        for j in range(len(self.reduce_dims)):
+            if self.reductions[j] == 'sum':
+                x = torch.sum(x, dim=self.reduce_dims[j], keepdim=True)
+            else:
+                x = torch.mean(x, dim=self.reduce_dims[j], keepdim=True)
+        
+        return x
+        
+    def abs(self, x, y, h=None):
+        """absolute H1 norm
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            inputs
+        y : torch.Tensor
+            targets
+        h : float or list, optional
+            normalization constant for reduction, by default None
+        """
+        #Assume uniform mesh
+        if h is None:
+            h = self.uniform_h(x)
+        else:
+            if isinstance(h, float):
+                h = [h]*self.d
+            
+        dict_x, dict_y = self.compute_terms(x, y, h)
+
+        const = math.prod(h)
+        diff = const*torch.norm(dict_x[0] - dict_y[0], p=2, dim=-1, keepdim=False)**2
+
+        for j in range(1, self.d + 1):
+            diff += const*torch.norm(dict_x[j] - dict_y[j], p=2, dim=-1, keepdim=False)**2
+        
+        diff = diff**0.5
+
+        if self.reduce_dims is not None:
+            diff = self.reduce_all(diff).squeeze()
+            
+        return diff
+        
+    def rel(self, x, y, h=None):
+        """relative H1-norm
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            inputs
+        y : torch.Tensor
+            targets
+        h : float or list, optional
+            normalization constant for reduction, by default None
+        """
+        #Assume uniform mesh
+        if h is None:
+            h = self.uniform_h(x)
+        else:
+            if isinstance(h, float):
+                h = [h]*self.d
+        
+        dict_x, dict_y = self.compute_terms(x, y, h)
+
+        diff = torch.norm(dict_x[0] - dict_y[0], p=2, dim=-1, keepdim=False)**2
+        ynorm = torch.norm(dict_y[0], p=2, dim=-1, keepdim=False)**2
+
+        for j in range(1, self.d + 1):
+            diff += torch.norm(dict_x[j] - dict_y[j], p=2, dim=-1, keepdim=False)**2
+            ynorm += torch.norm(dict_y[j], p=2, dim=-1, keepdim=False)**2
+
+        dx = 0.01
+        dy = 0.01
+        left_boundary_dx = (x[:, :, 1, :] - x[:, :, 0, :]) / dx
+        right_boundary_dx = (x[:, :, 31, :] - x[:, :, 30, :]) / dx
+        top_boundary_dy = (x[:, :, :, 1] - x[:, :, :, 0]) / dy
+        bottom_boundary_dy = (x[:, :, :, 31] - x[:, :, :, 30]) / dy
+        left_boundary_dx_squared = left_boundary_dx ** 2
+        right_boundary_dx_squared = right_boundary_dx ** 2
+        top_boundary_dy_squared = top_boundary_dy ** 2
+        bottom_boundary_dy_squared = bottom_boundary_dy ** 2
+        sum_squares_dx_left = torch.sum(left_boundary_dx_squared)
+        sum_squares_dx_right = torch.sum(right_boundary_dx_squared)
+        total_sum_squares_dx = sum_squares_dx_left + sum_squares_dx_right
+        sum_squares_dy_top = torch.sum(top_boundary_dy_squared)
+        sum_squares_dy_bottom = torch.sum(bottom_boundary_dy_squared)
+        total_sum_squares_dy = sum_squares_dy_top + sum_squares_dy_bottom
+        total_sum_squares = total_sum_squares_dx + total_sum_squares_dy
+        diff = diff + total_sum_squares
+        
+        diff = (diff**0.5)/(ynorm**0.5)
+
+        if self.reduce_dims is not None:
+            diff = self.reduce_all(diff).squeeze()
+            
+        return diff
+
+    def __call__(self, y_pred, y, h=None, **kwargs):
+        """
+        Parameters
+        ----------
+        y_pred : torch.Tensor
+            inputs
+        y : torch.Tensor
+            targets
+        h : float or list, optional
+            normalization constant for reduction, by default None
+        """
+        return self.rel(y_pred, y, h=h)
 
 class PointwiseQuantileLoss(object):
     def __init__(self, alpha, reduce_dims = 0, reductions='sum'):
